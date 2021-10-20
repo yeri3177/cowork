ec90b623178283b84773101ecae4aadfcfecc67e
=yeri3177 <yeri3177@gmail.com>
2021-10-20 13:52:50 +0900

    HelloJungyeri 클래스 추가!

---
+++ src/com/kh/cowork/HelloJungyeri.java
@@ -0,0 +1,11 @@
+package com.kh.cowork;
+
+public class HelloJungyeri {
+
+	/**
+	 * @author 정예리 
+	 */
+	public void hello() {
+		System.out.println("안녕하세요~ HelloJungyeri 클래스의 hello메소드 실행! ");
+	}
+}
